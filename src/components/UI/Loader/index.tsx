64b4dc416fa64af6a901291e864fbf96844d90c6
Gökhan Demirer <gdmr34@gmail.com>
2024-03-17 09:57:17 +0300

    Add Loader component and styles

---
+++ src/components/UI/Loader/index.tsx
@@ -0,0 +1,9 @@
+import { LoaderContainer, StyledLoader } from "./styles";
+
+export default function Loader() {
+  return (
+    <LoaderContainer>
+      <StyledLoader />
+    </LoaderContainer>
+  );
+}
